53d3086ae5b3cf4006e650adf209749b8c50ddcf
AbhiAgarwal1005 <Abhiagarwal3219@gmail.com>
2025-06-01 22:49:23 +0530

    new

--- src/components/clinic-figures/figure-card/index.tsx
+++ src/components/clinic-figures/figure-card/index.tsx
@@ -1,15 +1,19 @@
 import { IoLocationOutline } from "react-icons/io5";
 import { NumberTicker } from "../../ui/NumberTicker";
+import { BorderBeam } from "../../ui/BorderBeam";
 
 const FigureCard = () => {
     return (
-        <div className="bg-primary shadow-md rounded-lg p-6 flex flex-col items-center w-full gap-1.5">
-            <IoLocationOutline size={50} color="#FFEECB"/>
+        <div className="bg-white shadow-md rounded-lg p-6 py-10 flex flex-col items-center w-full gap-1.5 relative overflow-hidden">
+            <IoLocationOutline size={50} color="#DFC57B" />
             <NumberTicker
                 value={100}
                 className="text-textPrimary text-3xl font-bold"
             />
             <p className="text-xl font-medium text-textPrimary">Clinic Visits</p>
+            <BorderBeam duration={4}
+                size={300}
+                className="from-transparent via-primary to-secondary" />
         </div>
     )
 };
